4a7ca6b1f2b84a6dab07c1512ea5c47fd775a281
Robert Rodgers <Robert_L_Rodgers@progressive.com>
2025-01-21 15:02:06 -0500

    before break

--- src/app/syntax/01-types.spec.ts
+++ src/app/syntax/01-types.spec.ts
@@ -1,8 +1,27 @@
-describe('Basic Jasmine Tests', () => {
-  it('Can Add Two Numbers', () => {
-    // const myName = "Robert";
-    let myAge = 25;
-    // @ts-expected
-    myAge = 'Old';
+/* eslint-disable @typescript-eslint/no-unused-vars */
+/* eslint-disable prefer-const */
+describe('Basic Types', () => {
+  it('Implicit Types', () => {
+    const num1 = 12;
+    const num2 = 10;
+    const answer = num1 + num2;
+
+    let age: string | number;
+    age = 'older';
+    age = 'older even';
+
+    let friends: string[];
+
+    friends = ['Bob', 'Sue', 'Joe'];
+
+    let luckyNumbers = getLuckyNumbers();
+
+    expect(age).toBeDefined();
+
+    expect(answer).toBe(22); // this will be a failing until we fix it which we will do so.
   });
 });
+
+function getLuckyNumbers() {
+  return [1, 8, 20, 108];
+}
