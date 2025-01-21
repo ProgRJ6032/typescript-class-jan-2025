9434bac00a2aa235f72a8c75b8636cf338b2d921
Robert Rodgers <Robert_L_Rodgers@progressive.com>
2025-01-21 14:09:34 -0500

    Initial

---
+++ src/app/syntax/01-types.spec.ts
@@ -0,0 +1,8 @@
+describe('Basic Jasmine Tests', () => {
+  it('Can Add Two Numbers', () => {
+    // const myName = "Robert";
+    let myAge = 25;
+    // @ts-expected
+    myAge = 'Old';
+  });
+});
